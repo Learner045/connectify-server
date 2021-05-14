089ff8cfdfd52cde831ec8e2949517cb6d05f2c6
Learner045 <shreya.ds045@gmail.com>
2021-05-14 00:30:21 -0400

    fix module2

--- GraphQL/resolvers/users.js
+++ GraphQL/resolvers/users.js
@@ -1,7 +1,7 @@
 const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const {SECRET_KEY} = require('../../config');
+//const {SECRET_KEY} = require('../../config');
 const { UserInputError } = require('apollo-server');
 const {
     validateRegisterInput,
